caeff707c2398e49c5299c7041bc8a29db4fd95b
remi-turini <71329993+remi-turini@users.noreply.github.com>
2022-10-20 21:32:18 +0200

    delete

--- back/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const sensorController = require("../controllers/sensor.controller")
-
-router.get("/", sensorController.getAllValues);
-router.post("/", sensorController.registerValue);
-
-module.exports = router;
--- back/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./app");
-const port = 3001;
-
-const server = http.createServer(app);
-
-server.listen(port,() => {
-    console.log("Server up and running");
-})
